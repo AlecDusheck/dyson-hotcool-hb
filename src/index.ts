47799315fcba73da2ae3982f4092b8c2375514ee
adusheck <alec@simplyalec.com>
2023-04-15 14:21:20 -0500

    changes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import { API } from 'homebridge';
 import * as constants from "./constants";
 import {DysonBP01} from "./accessory";
 
-/**
- * This method registers the platform with Homebridge
- */
 export = (api: API) => {
     api.registerAccessory(constants.ACCESSORY_NAME, DysonBP01);
 };
